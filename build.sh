aeee393e960732b939a427fd637fff04f8dc5c60
Joshua LaRossa <josh.larossa26@gmail.com>
2020-04-28 15:47:58 -0400

    Updated Dockerfile

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-IMAGE_NAME=cc-app-test
+IMAGE_NAME=cc-app
 CONTAINER_ID=$(docker ps -a | awk "/$IMAGE_NAME/"'{ print $1 }')
 docker stop ${CONTAINER_ID}
 docker rm ${CONTAINER_ID}
 docker rmi ${IMAGE_NAME}
-docker build -t cc-app .
-docker run -d --name cc-app-test -p 80:80 cc-app
-
+docker build -t ${IMAGE_NAME} .
+docker run -d --rm --name cc-app ${IMAGE_NAME}
