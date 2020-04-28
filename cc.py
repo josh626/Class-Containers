c016998006b6f580692350dac3c1ad11cc7e759e
josh626 <josh.larossa26@gmail.com>
2020-04-28 13:31:23 +0000

    Updates and added templates for information

--- cc.py
+++ cc.py
@@ -11,16 +11,50 @@ cc_projects = cc_get.get_projects()
 def index():
     return render_template('homepage.html', cc_projects=cc_projects, value1="This is a Rendered Value!!!")
 
+
+@cc.route('/access/')
+def access():
+    return render_template('AccessCC.html', cc_projects=cc_projects)
+
+@cc.route('/dockerinfo/')
+def dockerinfo():
+    return render_template('dockerinfo.html', cc_projects=cc_projects)
+
+@cc.route('/containerimages/')
+def containerimages():
+    return render_template('Container_images.html', cc_projects=cc_projects)
+
+@cc.route('/howtousedocker/')
+def howtousedocker():
+    return render_template('HowToUseDocker.html', cc_projects=cc_projects)
+
+@cc.route('/launching/')
+def launching():
+    return render_template('L_S_Containers.html', cc_projects=cc_projects)
+
+@cc.route('/resources/')
+def resources():
+    return render_template('resources.html', cc_projects=cc_projects)
+
 @cc.route('/project/<project_id>')
 def show_project(project_id):
     # TODO: add if for project_id empty --> route to '/'
     #       Render child template for list of projects (including any markdown) 
     #       assign rendered_project_child template to content 
     repos = cc_get.get_repos(project_id)
+
+    project_id = int(project_id)
+    project_list = cc_get.get_projects()
+    project_data = {}
+    for project in project_list:
+        if project['project_id'] is project_id:
+            project_data = project
+    project_name = project_data['name']
     return render_template('class.html',
                            cc_projects=cc_projects,
                            cc_project_id=project_id,
-                           cc_repos=repos)
+                           cc_repos=repos,
+                           cc_project_name=project_name,)
 
 @cc.route('/repo/<project_id>/<repo_id>')
 def show_repo(project_id, repo_id):
@@ -30,7 +64,7 @@ def show_repo(project_id, repo_id):
     for repo in repo_list:
         if repo['id'] is repo_id:
             repo_data = repo
-    
+
     repo_json_pretty = json.dumps(repo_data, sort_keys = True, indent = 4, separators = (',', ': '))
     repo_name = repo_data['name']
     repo_description = repo_data['description']
@@ -47,7 +81,6 @@ def show_repo(project_id, repo_id):
                                    )
     # TODO: add if for repo empty --> route to '/'
     #       Get label data
-    #       Generate docker pull commands
     #       Render child template using values from repo (markdown, Name, docker pull commands)
     #       assign rendered_repo_child template to content 
 
